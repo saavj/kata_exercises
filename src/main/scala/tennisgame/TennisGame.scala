df0ab5423ce22e11e03e5642058341ae7ea42b14
Sofia Vaughan-Jones <sofiavaughanjones@itv.com>
2024-05-07 15:30:25 +0100

    wip

--- src/main/scala/tennisgame/TennisGame.scala
+++ src/main/scala/tennisgame/TennisGame.scala
@@ -11,3 +11,34 @@ class TennisGame(player1: Player, player2: Player) {
 
 
 }
+
+
+
+//package tennisgame
+//
+//
+//trait Player{
+//  def incrementScore: Player
+//}
+//
+//case class Player1(name: String, score: Int) extends Player {
+//  def incrementScore: Player1 = {
+//    this.copy(score = score + 15)
+//  }
+//}
+//case class Player2(name: String, score: Int) extends Player {
+//  def incrementScore: Player2 = {
+//    this.copy(score = score + 15)
+//  }
+//}
+//
+//
+//case class TennisGame(player1: Player1, player2: Player2) {
+//  def winPoint(player: Player) = {
+//    player match {
+//      case _: Player1 => new TennisGame(player1.incrementScore, player2)
+//      case _: Player2 => new TennisGame(player1, player2.incrementScore)
+//
+//    }
+//  }
+//}
